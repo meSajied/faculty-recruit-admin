a5a7a5986c538b7d7746c3cd7117ffd4e76ffcee
meSajied <sajiedshahyousuf@gmail.com>
2024-02-06 15:52:11 +0600

    fix: login form

--- src/account/Login.js
+++ src/account/Login.js
@@ -2,13 +2,15 @@ import React, {useState} from "react";
 
 import axios from "axios";
 import {useAuth} from "./Authentication";
-import {Navigate, useNavigate} from "react-router";
+import {Navigate} from "react-router";
 
 const Login = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [formData, setFormdata] = useState({
+    username: "",
+    password: ""
+  })
+  const [showWarning, setShowWarning] = useState(false);
 
-  const navigate = useNavigate();
   const {login, isLoggedIn} = useAuth();
 
   if(isLoggedIn) {
@@ -17,54 +19,92 @@ const Login = () => {
     )
   }
 
+  function handleChange(e) {
+    const {name, value} = e.target;
+
+    setFormdata((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  }
+
   return (
       <div className="flex items-center justify-center h-screen">
-      <form onSubmit={handleLogin} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-        <div className="mb-4">
-        <label htmlFor='text' className="block text-gray-700 text-sm font-bold mb-2">
-          Username:
-        </label>
-          <input type='text' name='username' className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              onChange={(e) => setUsername(e.target.value)} 
-              required
-          />
-        </div>
+        <div className="flex flex-col items-center">
+          {showWarning && (
+              <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded flex">
+                <p>Login failed. Please check your credentials and try again.</p>
+                <span
+                    className="float-right cursor-pointer"
+                    onClick={() => setShowWarning(false)}
+                >
+              &times;
+            </span>
+              </div>
+          )}
+        <form onSubmit={handleLogin} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+          <div className="mb-4">
+          <label htmlFor='text' className="block text-gray-700 text-sm font-bold mb-2">
+            Username:
+          </label>
+            <input type='text' name='username' className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                value={formData.username}
+                onChange={handleChange} 
+                required
+            />
+          </div>
 
-        <div className="mb-4">
-        <label htmlFor='password' className="block text-gray-700 text-sm font-bold mb-2">
-          Password:
-        </label>
-          <input type='password' name='password' className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              onChange={(e) => setPassword(e.target.value)} 
-              required
-          />
-        </div>
+          <div className="mb-4">
+          <label htmlFor='password' className="block text-gray-700 text-sm font-bold mb-2">
+            Password:
+          </label>
+            <input type='password' name='password' className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                value={formData.password}
+                onChange={handleChange} 
+                required
+            />
+          </div>
 
-        <div className="flex items-center justify-center">
-        <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-          Login
-        </button>
+          <div className="flex items-center justify-center">
+          <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+            Login
+          </button>
+          </div>
+        </form>
         </div>
-      </form>
       </div>
   )
 
-  async function handleLogin() {
-    await axios.get('http://localhost:4414/account/administration/login',
-        {
-          params: {
-            username: username,
-            password: password
-          }
-        })
+  async function handleLogin(e) {
+    e.preventDefault();
+
+    try {
+      await axios.post('http://localhost:4414/account/administration/login', formData, {
+        headers: {
+          "Content-Type": "application/json"
+        }
+      })
         .then(res => {
           if(res.data?.userName && res.data?.id){
             login(res.data);
-          } else {
-            navigate('/');
-            alert(res.data);
+          }else {
+            setShowWarning(true);
           }
+
+          clearData();
         });
+    }catch(e) {
+      console.log(e);
+      clearData();
+      setShowWarning(true);
+    } 
+  }
+
+  function clearData() {
+    setFormdata({
+      username: "",
+      password: ""
+    })
   }
 };
 
